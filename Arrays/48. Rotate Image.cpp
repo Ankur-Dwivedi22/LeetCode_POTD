6b95cbb289e22c3a3d13b3502682e7ef8d2ce4ab
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-05-05 00:28:07 +0530

    04/05/26

---
+++ Arrays/48. Rotate Image.cpp
@@ -0,0 +1,23 @@
+// Approach : Transpose + Reverse
+// TC : O(N * N)
+// SC : O(1)
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (auto &v : matrix)
+        {
+            reverse(v.begin(), v.end());
+        }
+    }
+};
