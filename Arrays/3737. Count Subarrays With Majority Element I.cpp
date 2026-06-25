0a49df0ff5e541b571b86f821fd31e3231e8dae9
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-06-25 21:55:10 +0530

    Arrays

---
+++ Arrays/3737. Count Subarrays With Majority Element I.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int countMajoritySubarrays(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int ct = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            unordered_map<int, int> mp;
+            for (int j = i; j < n; j++)
+            {
+                mp[nums[j]]++;
+                int len = j - i + 1;
+                if (mp[target] > (len / 2))
+                {
+                    ct++;
+                }
+            }
+        }
+
+        return ct;
+    }
+};
