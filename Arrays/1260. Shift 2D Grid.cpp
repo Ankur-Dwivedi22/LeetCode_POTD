514bf640e3601d350aff3db5abd8e2a4cba7b982
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-07-20 16:02:53 +0530

    Arrays

---
+++ Arrays/1260. Shift 2D Grid.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int r = grid.size(), c = grid[0].size();
+        int n = r * c;
+        k = k % n;
+
+        if(k == 0) return grid;
+
+        auto shift = [&](int i, int j){
+            while(i < j){
+                swap(grid[i / c][i % c], grid[j / c][j % c]);
+                i++;
+                j--;
+            }
+        };
+
+        shift(0, n-1);
+        shift(0, k-1);
+        shift(k, n-1);
+
+        return grid;
+    }
+};
