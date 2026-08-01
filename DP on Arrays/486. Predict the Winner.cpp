d3bfa76cbe04478200a0c07ceef0df07226fcb73
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-08-01 13:49:06 +0530

    DP

---
+++ DP on Arrays/486. Predict the Winner.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    bool predictTheWinner(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++)
+        {
+            dp[i][i] = nums[i];
+        }
+
+        for (int left = n - 1; left >= 0; left--)
+        {
+            for (int right = 0; right < n; right++)
+            {
+                if (left == right)
+                    continue;
+                int c1 = -1e9, c2 = -1e9;
+                if (left + 1 < n)
+                    c1 = nums[left] - dp[left + 1][right];
+                if (right - 1 >= 0)
+                    c2 = nums[right] - dp[left][right - 1];
+
+                dp[left][right] = max(c1, c2);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+};
